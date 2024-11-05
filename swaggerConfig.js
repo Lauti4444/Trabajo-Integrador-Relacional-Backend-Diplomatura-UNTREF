809f46681a6507f4ed5d78c456e3001921e63c78
Lauti4444 <lautarolopez4@hotmail.com>
2024-11-04 23:35:38 -0300

    era porke no taba comiteando el .env(aunque nose que tan seguro sea xd)

--- swaggerConfig.js
+++ swaggerConfig.js
@@ -12,7 +12,7 @@ const swaggerOptions = {
     servers: [
       {
         url: 'http://localhost:3000',
-        url: 'https://renewed-radiance-production.up.railway.app.up.railway.app'
+        url: 'https://trabajo-integrador-sql-production.up.railway.app/api-docs/'
       },
     ],
     components: {
